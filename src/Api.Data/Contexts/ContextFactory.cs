c67a2a0c5699ce628057c9f6f6f92fce17b4a54e
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2019-12-25 12:02:41 -0300

    Correcao string de conexao

--- src/Api.Data/Contexts/ContextFactory.cs
+++ src/Api.Data/Contexts/ContextFactory.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace Api.Data.Contexts {
     public class ContextFactory : IDesignTimeDbContextFactory<MyContext> {
         public MyContext CreateDbContext (string[] args) {
-            var connectionString = "Server=localhost;Port-3306;Database=dbAPI;Uid=marshall;Pwd=passwordTest";
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=marshall;Pwd=passwordTest";
             var optinsBuilder = new DbContextOptionsBuilder<MyContext> ();
             optinsBuilder.UseMySql (connectionString);
             return new MyContext (optinsBuilder.Options);
