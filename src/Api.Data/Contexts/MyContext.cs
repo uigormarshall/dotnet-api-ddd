7512d9463f66fd69905f0f393b2d98d97bf2db83
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2019-12-25 11:05:47 -0300

    Adiciona contexto

---
+++ src/Api.Data/Contexts/MyContext.cs
@@ -0,0 +1,15 @@
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Contexts {
+    public class MyContext : DbContext {
+        public DbSet<UserEntity> Users { get; set; }
+        public MyContext (DbContextOptions<MyContext> options) : base (options) { }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder) {
+
+            base.OnModelCreating (modelBuilder);
+        }
+    }
+
+}
