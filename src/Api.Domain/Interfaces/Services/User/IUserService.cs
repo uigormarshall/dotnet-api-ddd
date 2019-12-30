d7a8f866183d22d3743151bd2e4e27084eced9ae
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2019-12-30 16:41:44 -0300

    Corrige assinatura de metodo errada em UserInterface

--- src/Api.Domain/Interfaces/Services/User/IUserService.cs
+++ src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -6,7 +6,7 @@ using Api.Domain.Entities;
 namespace Api.Domain.Interfaces.Services.User {
     public interface IUserService {
         Task<UserEntity> Get (Guid id);
-        Task<IEnumerable<UserEntity>> GetAll (Guid id);
+        Task<IEnumerable<UserEntity>> GetAll ();
         Task<UserEntity> Post (UserEntity user);
         Task<UserEntity> Put (UserEntity user);
         Task<bool> Delete (Guid id);
