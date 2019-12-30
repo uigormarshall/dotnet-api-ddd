ccf46fa9996522eef1cdcf4d2328c9a6fdaca652
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2019-12-30 17:34:21 -0300

    Adiciona controller de usuario

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -4,19 +4,20 @@ using System.Threading.Tasks;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Application.Controllers {
+namespace Application.Controllers {
     [ApiController]
     [Route ("api/v1/users")]
     public class UsersController : ControllerBase {
         [HttpGet]
-        public async Task<ActionResult> GetAll ([FromServices] IUserService service)
+        public async Task<ActionResult> GetAll ([FromServices] IUserService service )
         {
-            if(!ModelState.IsValid) return BadRequest(ModelState);
-
-            try{
-               return  Ok(await service.GetAll());
-
-            }catch(ArgumentException e){
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
                 return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
             }
         }
